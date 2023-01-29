f22de739db5b6ee9f5e78eddc8162e577f1e9f57
sol6nik <80168295+sol6nik@users.noreply.github.com>
2023-01-29 23:02:12 +0300

    added user title

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,15 +1,30 @@
 import { UserStat } from "components/UserStat";
 import styles from "./UserCard.module.scss";
 import { LocalGitHubUser } from "types";
+import { UserTitle } from "components/UserTitle";
 
 interface UserCardProps extends LocalGitHubUser {}
 
-export const UserCard = (props: UserCardProps) => (
-  <div className={styles.userCard}>
-    <UserStat
-      repos={props.repos}
-      followers={props.followers}
-      following={props.following}
-    />
-  </div>
-);
+export const UserCard = (props: UserCardProps) => {
+  return (
+    <div className={styles.userCard}>
+      <img src={props.avatar} alt={props.login} className={styles.avatar} />
+
+      <UserTitle
+        created={props.created}
+        login={props.login}
+        name={props.name}
+      />
+
+      <p className={`${styles.bio} ? '' : ${styles.empty}`}>
+        {props.bio || "No bio provided"}
+      </p>
+
+      <UserStat
+        repos={props.repos}
+        followers={props.followers}
+        following={props.following}
+      />
+    </div>
+  );
+};
